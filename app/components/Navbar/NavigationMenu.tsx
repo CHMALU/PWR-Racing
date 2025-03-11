e87966e67b3d4887255f84d400c5ea495f43b27d
CHMALU <d.chmalu@gmail.com>
2025-03-11 12:36:04 +0100

    zmiana domylsnego w navie

--- app/components/Navbar/NavigationMenu.tsx
+++ app/components/Navbar/NavigationMenu.tsx
@@ -20,7 +20,7 @@ const NavigationMenu: React.FC<NavigationMenuProps> = ({ dict }) => {
       <ul className="flex space-x-4 h-full">
         <NavItem to="/">{dict.home}</NavItem>
         <NavItem to="/bolid/RT14e">{dict.bolid}</NavItem>
-        <NavItem to="/team/RT14e">{dict.team}</NavItem>
+        <NavItem to="/team/RT15e">{dict.team}</NavItem>
         <NavItem to="/about">{dict.about}</NavItem>
         <NavItem to="/partners">{dict.partners}</NavItem>
         <NavItem to="/news">{dict.news}</NavItem>
