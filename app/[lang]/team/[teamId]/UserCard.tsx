d9959a56f6c4fae2c2e2644ada364ccaab72a79f
CHMALU <d.chmalu@gmail.com>
2025-04-13 22:11:30 +0200

    member photo

--- app/[lang]/team/[teamId]/UserCard.tsx
+++ app/[lang]/team/[teamId]/UserCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import TileHeading from "./TileHeading";
 
 interface UserCardProps {
@@ -36,7 +36,8 @@ const UserCard: React.FC<UserCardProps> = ({
 }) => {
   const placeholderImage = "/images/TeamPlaceholder.jpg";
 
-  const memberImageSrc = `https://storage.googleapis.com/pwr-rt/${
+  // Generowanie ścieżki do obrazka członka zespołu
+  const generatedMemberImageSrc = `https://storage.googleapis.com/pwr-rt/${
     teamId === "RT11b" ? "RT11" : teamId
   }/${encodeURIComponent(
     member.name[0].toUpperCase() + member.name.slice(1)
@@ -44,16 +45,29 @@ const UserCard: React.FC<UserCardProps> = ({
     member.surname[0].toUpperCase() + member.surname.slice(1)
   )}.jpg`;
 
+  // Użycie stanu do dynamicznej zmiany obrazka w razie błędu
+  const [memberImageSrc, setMemberImageSrc] = useState(generatedMemberImageSrc);
+
+  const handleImgError = () => {
+    // Po błędzie ustawiamy ścieżkę do obrazka zastępczego
+    setMemberImageSrc(placeholderImage);
+  };
+
   return (
     <div className="relative group w-full">
       <div className="absolute inset-0 z-0 w-full h-full rounded-full blur-3xl bg-white opacity-10"></div>
       <div className="relative rounded-md overflow-hidden bg-black pb-14">
         <div className=" absolute w-full h-[350px]">
+          {/* Możesz wykorzystać obrazek zastępczy jako tło */}
           <Image
-            src={placeholderImage}
-            alt={`Zdjęcie pomocnicze`}
+            src="/images/TeamPlaceholder.jpg"
+            alt={`Zdjęcie ${member.name} ${member.surname}`}
             fill
             style={{ objectFit: "cover" }}
+            loading="lazy"
+            placeholder="blur"
+            blurDataURL={placeholderImage}
+            onError={handleImgError}
           />
         </div>
         <div className="relative h-[350px]">
@@ -62,6 +76,10 @@ const UserCard: React.FC<UserCardProps> = ({
             alt={`Zdjęcie ${member.name} ${member.surname}`}
             fill
             style={{ objectFit: "cover" }}
+            loading="lazy"
+            placeholder="blur"
+            blurDataURL={placeholderImage}
+            onError={handleImgError}
           />
         </div>
         <TileHeading
